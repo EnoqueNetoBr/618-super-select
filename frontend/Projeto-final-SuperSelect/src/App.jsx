c922e53778fe9be6b53f02fcb812e1a606f4e82b
IgorSilva1990 <igor.silva.dev1990@gmail.com>
2025-03-28 19:54:27 -0300

    Import do Bootstrap e adicionada dependencia: React-Bootstrap.

--- frontend/Projeto-final-SuperSelect/src/App.jsx
+++ frontend/Projeto-final-SuperSelect/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Bootstrap is working</h1>
     </>
   )
 }
