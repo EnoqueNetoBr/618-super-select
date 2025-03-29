1c3b26a35796aea8ea874f37a6901638aae1b102
unknown <githubbofc@gmail.com>
2025-03-28 21:17:52 -0300

    add estrutura do formulario

--- frontend/Projeto-final-SuperSelect/src/pages/cadastro/cadastro.jsx
+++ frontend/Projeto-final-SuperSelect/src/pages/cadastro/cadastro.jsx
@@ -2,7 +2,21 @@ import './cadastro.css'
 function Cadastro() {
     return (
         <>
-        
+        <div id='container'>
+            <h1>Criar Nova Conta</h1>
+            <hr />
+            <h3>Nome</h3>
+            <input type="text" />
+            <h3>Email</h3>
+            <input type="text" />
+            <h3>Senha</h3>
+            <input type="text" />
+            <br />
+            <br />
+            <button id='btn-criar-conta'>Criar Nova Conta</button>
+            <br />
+            <button id='btn-login'>Login</button>
+        </div>
         </>
     )
 }
