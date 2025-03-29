9f8786d821e50b0e6fc9c7c569a220dc0fd4ec5b
unknown <gabriel.teles10000@gmail.com>
2025-03-28 21:53:17 -0300

    Alterações no css

--- frontend/Projeto-final-SuperSelect/src/pages/cadastro/cadastro.jsx
+++ frontend/Projeto-final-SuperSelect/src/pages/cadastro/cadastro.jsx
@@ -1,23 +1,31 @@
 import './cadastro.css'
+
 function Cadastro() {
     return (
-        <>
-        <div id='container'>
-            <h1>Criar Nova Conta</h1>
-            <hr />
-            <h3>Nome</h3>
-            <input type="text" />
-            <h3>Email</h3>
-            <input type="text" />
-            <h3>Senha</h3>
-            <input type="text" />
-            <br />
-            <br />
-            <button id='btn-criar-conta'>Criar Nova Conta</button>
-            <br />
-            <button id='btn-login'>Login</button>
+        <div id='img-fundo'>
+            {/* elemento de imagem de fundo p - 0 */}
+
+
+
+            {/* formulario de cadastro p - 1 */}
+
+            <div id='container'>
+                <img src="" alt="" />
+                <h1>Criar Nova Conta</h1>
+                <hr />
+                <h3>Nome</h3>
+                <input type="text" />
+                <h3>Email</h3>
+                <input type="text" />
+                <h3>Senha</h3>
+                <input type="text" />
+                <br />
+                <br />
+                <button id='btn-criar-conta'>Criar Nova Conta</button>
+                <br />
+                <button id='btn-login'>Login</button>
+            </div>
         </div>
-        </>
     )
 }
 
