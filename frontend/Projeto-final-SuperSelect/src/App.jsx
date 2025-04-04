4abdb11fb933d91dc272fa1a6fd37664a328c06b
Alessandro Leonardi <a.leonardi1995@icloud.com>
2025-04-04 16:48:13 -0300

    Configured SASS adding some base colors to the Bootstrap theme-colors.

--- frontend/Projeto-final-SuperSelect/src/App.jsx
+++ frontend/Projeto-final-SuperSelect/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './sass/custom.css';
 import Dashboard from './pages/dashboard/dashboard';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Dashboard /> 
+      <Dashboard />
     </>
   )
 }
--- frontend/Projeto-final-SuperSelect/src/pages/dashboard/dashboard.jsx
+++ frontend/Projeto-final-SuperSelect/src/pages/dashboard/dashboard.jsx
@@ -1,39 +1,13 @@
-const styleMinHeight = { minHeight: "50px" };
+import './dashboard.css';
 
-export default function Dashboard() {
+function Dashboard() {
   return (
     <>
-      <div className="container-fluid bg-secondary vh-100">
-        <div className="row bg-primary h-100" style={styleMinHeight}>
-          <div className="col-3 bg-light p-0" style={styleMinHeight}>
-            {/* Pill Button Menu */}
-            <div
-              className="row bg-danger w-100 p-0 m-0 d-flex justify-content-center align-items-center"
-              style={styleMinHeight}
-            >
-              <div
-                className="col-10 btn btn-warning rounded-pill"
-                style={styleMinHeight}
-              >
-                {/* Pill Icon + Description */}
-                <div className="row bg-dark" style={styleMinHeight}>
-                  {/* Pill Icon Placeholder */}
-                  <div
-                    className="col-3 bg-primary"
-                    style={styleMinHeight}
-                  ></div>
-                  {/* Pill Description */}
-                  <div
-                    className="col bg-secondary"
-                    style={styleMinHeight}
-                  ></div>
-                </div>
-              </div>
-            </div>
-            {/*END Pill Button Menu */}
-          </div>
-        </div>
+      <div className='mt-5 d-flex justify-content-center aling-items-center'>
+        <h1>Hello world</h1>
       </div>
     </>
   );
 }
+
+export default Dashboard;
