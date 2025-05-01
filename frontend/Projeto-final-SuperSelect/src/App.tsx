d2840674e04b2c14a40f939b5c5dda863700e4cd
Alessandro <a.leonardi1995@icloud.com>
2025-05-01 16:52:52 -0300

    Adding Typescript support.

---
+++ frontend/Projeto-final-SuperSelect/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Página inicial do projeto.</h1>
+    </>
+  )
+}
+
+export default App
