aa5df90f2e678765b17e970e59783f26fec7d23d
Alessandro <a.leonardi1995@icloud.com>
2025-05-06 17:13:59 -0300

    Finish login page.

--- frontend/Projeto-final-SuperSelect/src/pages/loginPage/loginPage.jsx
+++ frontend/Projeto-final-SuperSelect/src/pages/loginPage/loginPage.jsx
@@ -1,8 +1,13 @@
+import backgroundImage from './../../images/background/signupPageBackground.png';
+
 export function LogInPage() {
   return (
     <>
-      <div className='container-fluid bg-secondary m-0 p-0 d-flex justify-content-center align-items-center' style={{ height: '75vh' }}>
+      <div className='container-fluid bg-secondary m-0 p-0 d-flex justify-content-center align-items-center' style={{ height: '75vh', position: 'relative' }}>
         <LogInForm />
+        <div style={{ position: 'absolute', top: '0px', bottom: '0px', right: '0px', left: '0px' }}>
+          <img className='w-100 h-100' src={backgroundImage} alt='Login background image.' style={{ objectFit: 'cover' }} />
+        </div>
       </div>
     </>
   );
@@ -11,17 +16,36 @@ export function LogInPage() {
 function LogInForm() {
   return (
     <>
-      <div className='container m-0 p-0 bg-light rounded-5' style={{ width: '30rem', height: '30rem' }}>
-        <form method='' action=''>
-          <div className='container m-0 p-0 p-4'>
-            <label htmlFor='loginUserEmail'>Endereço de email: </label>
-            <input className='form-control' type='mail' name='loginUserEmail' id='loginUserEmail' />
-            <label htmlFor='loginPassword'>Senha:</label>
-            <input className='form-control' type='password' name='loginPassword' id='loginPassword' />
+      <div className='container m-0 p-0 rounded-5 shadow-lg' style={{ width: '30rem', height: '30rem', zIndex: '10', backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <form className='h-100' method='' action=''>
+          <div className='container m-0 p-0 p-4 d-flex flex-column justify-content-center align-items-around h-100'>
+            <div className='mt-3'>
+              <label htmlFor='loginUserEmail'>
+                <p className='fs-5'>Endereço de email: </p>
+              </label>
+              <input className='form-control shadow-sm' type='mail' name='loginUserEmail' id='loginUserEmail' />
+            </div>
 
-            <button type='submit' style={{ all: 'unset' }}>
+            <div className='mt-3'>
+              <label htmlFor='loginPassword'>
+                <p className='fs-5'>Senha: </p>
+              </label>
+              <input className='form-control shadow-sm' type='password' name='loginPassword' id='loginPassword' />
+            </div>
+
+            <button className='mt-auto' type='submit' style={{ all: 'unset' }}>
               <LogInSubmitButton />
             </button>
+
+            <div className=''>
+              <p className='text-center mb-1'>
+                Não possui uma conta? <span className='text-primary'>Cadastre-se.</span>{' '}
+              </p>
+
+              <div>
+                <CreateAccountButton />
+              </div>
+            </div>
           </div>
         </form>
       </div>
@@ -33,6 +57,25 @@ function LogInSubmitButton() {
   return (
     <>
       <div className='bg-primary rounded-pill mx-auto my-3 p-0 button shadow-sm' style={{ width: '27rem', height: '4rem' }}>
+        <div className='row h-100 m-0 p-0'>
+          {/* Google icon */}
+          <div className='col-3 d-flex justify-content-center align-items-center m-0 p-0 '>
+            {/* SVG */}
+            <i className='bi bi-person-circle text-light' style={{ fontSize: '2.6rem' }}></i>
+          </div>
+          <div className='col h-100 m-0 p-0 d-flex flex-column justify-content-center aling-items-center'>
+            <p className='fs-3 m-0 p-0 text-center text-light'>Entrar</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CreateAccountButton() {
+  return (
+    <>
+      <div className='bg-primary rounded-pill mx-auto mb-3 p-0 button shadow-sm' style={{ width: '27rem', height: '4rem' }}>
         <div className='row h-100 m-0 p-0'>
           {/* Google icon */}
           <div className='col-3 d-flex justify-content-center align-items-center m-0 p-0 '>
